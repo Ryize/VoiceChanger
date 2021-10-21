7f32ddc0307c48e5dcbc9725dee889a54d999aed
Чекашов Матвей Павлович <chekashovmatvey@gmail.com>
2021-10-21 22:26:46 +0300

    Update file_worker.py

--- file_worker.py
+++ file_worker.py
@@ -3,7 +3,6 @@ import os
 def check_exist_directory():
     if os.listdir().count('voice'):
         return True
-    return False
 
 def create_directory_with_save_voice():
     os.makedirs('voice/oga/')
